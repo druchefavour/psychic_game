91603f0dbc3efd563be84a8cb6f0c46aec2aba1c
druchefavour <druchefavour@gmail.com>
2016-11-05 17:58:34 -0500

    COMMIT1

--- assets/js/psychic.js
+++ assets/js/psychic.js
@@ -1,63 +1,80 @@
-// Initiate variables to be used in the game
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-    'i','j','k','l','m','n','o','p','r','s','t','u','v','w',
-    'x','y','w','x','y','z'];	// array of alphabets available for choice by the user and the computer
-var leftGuesses;                // guesses that are left after pressing a key
-var counter ;                   // Count user guesses
-var guesses = []; 
+// Define variables
+    // Initiate variables to be used in the game
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g','h','i','j','k','l','m','n','o','p','r','s','t','u','v','w','x','y','w','x','y','z'];   
+    // array of alphabets available for choice by the user and the computer
+    var guesses = [];               // Arrey of user's guesses 
+    var maxGuesses = 10             // Max. number of guesses allowed in a game
+    // Variables for tracking our wins, losses. They begin at 0.
+    var wins = 0;
+    var losses = 0;
 
-document.onkeyup = function(event) {
+document.onkeyup = function(event) {    //function to initiate game by pressing any key
 
-        // Determine which key was pressed, make it lowercase, and set it to the userGuess variable.
+    // Determine which key was pressed, make it lowercase, and set it to the userGuess variable.
+
         var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-        document.getElementById("demo").innerHTML = userGuess ;
+       // Pick out each key character 
         var i, len, text;  
         for (i = 0, len = userGuess.length, text = ""; i < len; i++) {
-        text += userGuess[i] + "<br>";
+        text += userGuess[i] + "<br>"
+        document.getElementById("psychicGame").innerHTML;
 
-        // Sets the computerGuess variable equal to a random choice from the computerChoice array.
-        var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-        for (j = 0, lenc = computerGuess.length, txt = ""; j < len; j++) {
-        txt += computerGuess[j] + "<br>";
-        document.getElementById("demo").innerHTML = computerGuess ;
-        var j, lenc, text1; 
 
-        // Write function to reload page after winning or losing
-            function refreshPage() {
-            window.location.reload();
-            };
+        // Sets the computer guess variable equal to a random choice from the computerChoice array.
+    var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+    var j, lenc, txt;
+    for (j = 0, lenc = computerGuess.length, txt = ""; j < lenc; j++) {
+    txt += computerGuess[j] + "<br>"
+        
+////////////////////////////////////////////////////////////////////////////
+     
+    var leftGuesses = 10 - guesses.length;   // guesses that are left after pressing a key
+    //var k, leng, num;
+    //for (k = 0, leng = 10, num = ""; k < leng; k++) {
+    //    num += leftGuesses + "<br>"
 
-            function addFunction() {    
-            var counter = 0;
-            return counter += 1;
-            };            
-        // Write function to show remaining guesess
-      
-        if (guesses.length <= 10){
+    if((0 < leftGuesses < 10) && (computerGuess == computerGuess[j]) && (userGuess == userGuess[i]) && (userGuess[i] != computerGuess[j])){
         guesses.push(userGuess[i]);
-        document.getElementById("demo").innerHTML = guesses;
+        document.getElementById("myGuesses").innerHTML = "User Guesses :"  +  guesses;
+        document.getElementById("compGuesses").innerHTML = "Computer Guesses :" + computerGuess;
+        document.getElementById("leftGuesses").innerHTML = "Your have " + leftGuesses + " guesses left";
 
-        if (guesses.length <=10) {
-            var leftGuesses;
-            leftGuesses =10-guesses.length;
-            //alert("Your are left with " + leftGuesses + " guesses");
-            document.getElementById("myLeftGuesses").innerHTML = ("Your have " + leftGuesses + " guesses left");
-          
-        if ((guesses.length <= 10) && (userGuess === userGuess[i]) && (computerGuess === computerGuess[j]) && (userGuess[i] === computerGuess[j]) ) {
-           		var Wins = 0; 
-           		wins += 1; 
-           		alert("You Win, Congratulations!");
-                 document.getElementById("congrats").innerHTML = (Wins + "Wins");       
-           
-    }
-        else if (guesses.length >= 9) {
-       alert("You Lost, Maybe Try Again"); 
-        document.getElementById("repeat").innerHTML = ("DON'T WORRY - PLAY AGAIN!");
-                   
-    }    
-           
+    if (leftGuesses <= 0){
+        losses ++;
+        alert("You lost, Maybe Try Again");
+        prompt("Do you want to play again");
+        guesses = [];
+        }
+
+    else if ((userGuess[i] == computerGuess[j])){
+         wins++;
+         alert("You Win, Congratulations!");
+         } 
+         
+    // Create new elements to write Computer Choice in HTML
+    //var targetDiv = document.getElementById("psychicGame");
+    //targetDiv.setAttribute("class" , "fancy"); 
+    //targetDiv.innerHTML = "Start Game";
+    //Create a new element to write User Choice in HTML
+    //var userGuess = document.createElement("div");
+    //userGuess.innerHTML = "<br> The User's Choice <br>";
+    //targetDiv.appendChild(userGuess);
+
+  //  var computerGuess = document.createElement("div");
+  //  computerGuess.innerHTML = "<br> My Computer Choice <br>";
+  //  targetDiv.appendChild(computerGuess);
+   //     var targetDiv = document.getElementById("myGuesses");
+ //Create a new element to write User Choice in HTML
+
+  document.getElementById("wins").innerHTML = "<p>Record Results!</p>" +
+  "<p>wins: " + wins + "</p>" +
+  "<p>losses: " + losses + "</p>";
+
+          // Injecting the HTML we just created into our div and updating the game information on our page.
+          document.querySelector("#wins").innerHTML = html;
+ }
+ }
+ }
 }
-}
- }
- }
- }
+    
+    
